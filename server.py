c46629a90d3a91d308af081e999b376451b254d3
DaniaK8 <daniak@skillup.online>
2023-07-24 07:21:47 -0400

    completed

--- server.py
+++ server.py
@@ -1,7 +1,3 @@
-''' Executing this function initiates the application of sentiment
-    analysis to be executed over the Flask channel and deployed on
-    localhost:5000.
-'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -10,12 +6,16 @@ app = Flask("Emotion Detector")
 @app.route("/emotionDetector")
 def sent_detector():
     text_to_analyze = request.args.get('textToAnalyze')
-    response = emotion_detector(text_to_analyze)
-    return "The given text has been identified as {} .".format(response)
+    dominant_emotion = emotion_detector(text_to_analyze)
+
+    if dominant_emotion is None:
+        return "Invalid text! Please try again."
+    else:
+        return "The given text has been identified as {}.".format(dominant_emotion)
 
 @app.route("/")
 def render_index_page():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=3001)
